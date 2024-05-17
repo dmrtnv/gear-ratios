4c9e0718316785c89e2edf9cffaca4c2b702132d
dmrtnv <dimasmart2000@gmail.com>
2024-05-17 18:25:39 +0300

    Added drivetrainSlice

---
+++ src/store/features/drivetrain/drivetrainSlice.tsx
@@ -0,0 +1,58 @@
+import { COLORS } from '@/types/Color';
+import { Drivetrain } from '@/types/Drivetrain';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialState: Drivetrain[] = [];
+
+export const drivetrainSlice = createSlice({
+  name: 'drivetrain',
+  initialState,
+  reducers: {
+    set: (state, action: PayloadAction<{ crankset: number[]; cassette: number[] }[]>) => {
+      state.splice(0, state.length);
+
+      action.payload.forEach((d, i) => {
+        state.push({
+          ...d,
+          id: uuidv4(),
+          name: 'Drivetrain ' + (i + 1),
+          ridingStyle: 'road',
+          wheelSize: '28',
+          color: COLORS[i],
+        });
+      });
+    },
+    addNew: (state) => {
+      state.push({
+        id: uuidv4(),
+        name: `Drivetrain ${state.length + 1}`,
+        ridingStyle: 'road',
+        wheelSize: '28',
+        color: COLORS[0],
+        cassette: [],
+        crankset: [],
+      });
+    },
+    remove: (state, action: PayloadAction<string>) => {
+      const index = state.findIndex((d) => d.id === action.payload);
+      if (index !== -1) {
+        state.splice(index, 1);
+
+        state.forEach((d, i) => {
+          d.name = `Drivetrain ${i + 1}`;
+        });
+      }
+    },
+    update: (state, action: PayloadAction<Drivetrain>) => {
+      const { id } = action.payload;
+      const existingDrivetrain = state.find((d) => d.id === id);
+
+      if (existingDrivetrain) {
+        Object.assign(existingDrivetrain, action.payload);
+      }
+    },
+  },
+});
+
+export const drivetrainReducer = drivetrainSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { drivetrainReducer } from './features/drivetrain/drivetrainSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    drivetrainReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
