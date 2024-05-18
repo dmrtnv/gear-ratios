27e522328c824c8644549a2bcda3b4a252135116
dmrtnv <dimasmart2000@gmail.com>
2024-05-18 17:51:38 +0300

    Changed searchparams url pattern

--- src/lib/parseDrivetrain.ts
+++ src/lib/parseDrivetrain.ts
@@ -1,13 +1,15 @@
 export function parseDrivetrain(
-  drivetrainString: string
+  drivetrainString: string,
 ): { success: true; data: { cassette: number[]; crankset: number[] } } | { success: false } {
-  const drivetrainRegEx = /^\d{1,2}((?:,\d{1,2}){0,2})x\d{1,2}((?:,\d{1,2}){0,14})$/;
+  const drivetrainRegEx = /^\d{1,2}((?:-\d{1,2}){0,2})x\d{1,2}((?:-\d{1,2}){0,14})$/;
 
   if (drivetrainRegEx.test(drivetrainString)) {
     const [cranksetString, cassetteString] = drivetrainString.split('x');
 
-    const crankset = [...new Set(JSON.parse(`[${cranksetString}]`) as number[])].filter((c) => c !== 0);
-    const cassette = [...new Set(JSON.parse(`[${cassetteString}]`) as number[])].filter((c) => c !== 0);
+    console.log(drivetrainString);
+
+    const crankset = [...new Set(cranksetString.split('-').map((c) => parseInt(c)))].filter((c) => c !== 0);
+    const cassette = [...new Set(cassetteString.split('-').map((c) => parseInt(c)))].filter((c) => c !== 0);
 
     return { success: true, data: { crankset, cassette } };
   }
