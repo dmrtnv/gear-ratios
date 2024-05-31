b77b46d30a670e1c29df8cd7318c5d47f5609b82
dmrtnv <dimasmart2000@gmail.com>
2024-05-31 21:07:17 +0300

    Created LinkButton

--- src/components/RidingStyleSelect.tsx
+++ src/components/RidingStyleSelect.tsx
@@ -3,14 +3,15 @@ import { cn } from '@/lib/utils';
 import { drivetrainSlice } from '@/store/features/drivetrain/drivetrainSlice';
 import { Drivetrain } from '@/types/Drivetrain';
 import { RIDING_STYLES, RidingStyle } from '@/types/RidingStyle';
+import _ from 'lodash';
 import { Link2, Link2Off } from 'lucide-react';
+import { useCallback, useEffect, useState } from 'react';
 
 type RidingStyleSelectProps = {
   drivetrain: Drivetrain;
 };
 
 function RidingStyleSelect({ drivetrain }: RidingStyleSelectProps) {
-  const link = useAppSelector((state) => state.drivetrain.options.linkRidingStyle);
   const { toggleLinkRidingStyle, updateRidingStyle } = drivetrainSlice.actions;
   const dispatch = useAppDispatch();
 
@@ -41,11 +42,45 @@ function RidingStyleSelect({ drivetrain }: RidingStyleSelectProps) {
           </label>
         ))}
 
-        <button className='ml-2 mt-1' onClick={() => dispatch(toggleLinkRidingStyle(drivetrain.id))}>
-          {link ? <Link2 size={18} strokeWidth={2} /> : <Link2Off size={18} strokeWidth={2} />}
-        </button>
+        <LinkButton toggle={() => dispatch(toggleLinkRidingStyle(drivetrain.id))} />
       </div>
     </fieldset>
+  );
+}
+
+function LinkButton({ toggle }: { toggle: () => void }) {
+  const link = useAppSelector((state) => state.drivetrain.options.linkRidingStyle);
+  const [active, setActive] = useState(false);
+
+  const deboucedSetDisabled = useCallback(
+    _.debounce(() => setActive(false), 2000),
+    [],
+  );
+
+  return (
+    <div className='flex items-center justify-center gap-3'>
+      <button
+        className='ml-2 mt-1'
+        onClick={() => {
+          toggle();
+          setActive(true);
+          deboucedSetDisabled();
+        }}
+      >
+        {link ? <Link2 size={18} strokeWidth={2} /> : <Link2Off size={18} strokeWidth={2} />}
+      </button>
+
+      <div className='flex items-center justify-center'>
+        <span className='mr-1'>/</span>
+        <div
+          data-state={active ? 'enabled' : 'disabled'}
+          className='overflow-hidden text-nowrap transition-[width] duration-1000 ease-out data-[state=active]:w-[100px] data-[state=disabled]:w-0'
+        >
+          {link ? <span>riding styles linked</span> : <span>riding styles unlinked</span>}
+        </div>
+        <span className='ml-1'>/</span>
+      </div>
+    </div>
   );
 }
 
