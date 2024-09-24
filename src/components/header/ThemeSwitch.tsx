a8b1927bb85b116434842e2578c290d87d237ae3
dmrtnv <dimasmart2000@gmail.com>
2024-09-24 12:29:44 +0200

    Refactor ThemeSwitch component

--- src/components/header/ThemeSwitch.tsx
+++ src/components/header/ThemeSwitch.tsx
@@ -1,44 +1,62 @@
-import { useTheme } from '@/contexts/ThemeProvider';
+import { Theme, useTheme } from '@/contexts/ThemeProvider';
 import { cn } from '@/lib/utils';
 import { Moon, Sun } from 'lucide-react';
+import React, { useCallback } from 'react';
 
 function ThemeSwitch({ className = '', ...props }: { className?: string }) {
   const { theme, setTheme } = useTheme();
 
+  const handleThemeChange = useCallback(
+    (newTheme: Theme) => {
+      if (theme !== newTheme) setTheme(newTheme);
+    },
+    [theme, setTheme],
+  );
+
   return (
-    <form className={cn('flex items-center justify-center gap-4 text-muted-foreground', className)} {...props}>
-      <input
-        onChange={() => setTheme('light')}
-        checked={theme === 'light'}
-        name='theme'
-        id='light'
-        type='radio'
-        className='peer/light sr-only'
-      />
-      <label
-        className='flex cursor-pointer items-center gap-1.5 rounded-md px-3 py-1 hover:bg-muted-md peer-checked/light:cursor-default peer-checked/light:bg-muted-lg'
-        htmlFor='light'
-      >
+    <fieldset
+      autoFocus
+      aria-label='Theme switcher'
+      className={cn('flex items-center justify-center gap-4 text-muted-foreground', className)}
+      {...props}
+    >
+      <legend className='sr-only'>Choose theme</legend>
+
+      <ThemeSwitchInput id='light' checked={theme === 'light'} onChange={() => handleThemeChange('light')}>
         <Sun size={16} strokeWidth={2} />
         <span>light</span>
-      </label>
+      </ThemeSwitchInput>
 
-      <input
-        onChange={() => setTheme('dark')}
-        checked={theme === 'dark'}
-        name='theme'
-        id='dark'
-        type='radio'
-        className='peer/dark sr-only'
-      />
-      <label
-        className='flex cursor-pointer items-center gap-1.5 rounded-md px-3 py-1 hover:bg-muted-md peer-checked/dark:cursor-default peer-checked/dark:bg-muted-lg'
-        htmlFor='dark'
-      >
+      <ThemeSwitchInput id='dark' checked={theme === 'dark'} onChange={() => handleThemeChange('dark')}>
         <Moon size={16} strokeWidth={2} />
         <span>dark</span>
+      </ThemeSwitchInput>
+    </fieldset>
+  );
+}
+
+type ThemeSwitchInputProps = {
+  checked: boolean;
+  id: string;
+  onChange: () => void;
+  children: React.ReactNode;
+  className?: string;
+};
+
+function ThemeSwitchInput({ id, checked, onChange, children, className = '' }: ThemeSwitchInputProps) {
+  return (
+    <div>
+      <input id={id} checked={checked} onChange={onChange} name='theme' type='radio' className='peer sr-only' />
+      <label
+        className={cn(
+          'flex cursor-pointer items-center gap-1.5 rounded-md px-3 py-1 hover:bg-muted-md peer-checked:cursor-default peer-checked:bg-muted-lg peer-focus:outline',
+          className,
+        )}
+        htmlFor={id}
+      >
+        {children}
       </label>
-    </form>
+    </div>
   );
 }
 
