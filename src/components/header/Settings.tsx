16577bde0ff74bca7f66176e4d2baf8e5e373287
dmrtnv <dimasmart2000@gmail.com>
2024-09-24 18:44:59 +0200

    Add overlay and focus lock to Settings dialog

--- src/components/header/Settings.tsx
+++ src/components/header/Settings.tsx
@@ -1,14 +1,22 @@
 import { SettingsIcon } from 'lucide-react';
+import React, { useEffect, useRef } from 'react';
 import { SettingsProvider, useSettings } from './SettingsContext';
 import ThemeSwitch from './ThemeSwitch';
-import { useEffect, useRef } from 'react';
+import { cn } from '@/lib/utils';
 
 function Settings() {
   return (
     <SettingsProvider>
-      <div className='relative border'>
+      <div className='relative'>
         <SettingsTrigger />
-        <SettingsContent />
+
+        <SettingsContent>
+          <h3 className='font-bold text-muted-foreground'>Theme</h3>
+
+          <ThemeSwitch className='mt-1' />
+        </SettingsContent>
+
+        <Overlay />
       </div>
     </SettingsProvider>
   );
@@ -18,7 +26,6 @@ function SettingsTrigger() {
   const { state, setState } = useSettings();
 
   const handleClick = () => {
-    // if (state === 'open') setState('closed');
     if (state === 'closed') setState('open');
   };
 
@@ -36,7 +43,18 @@ function SettingsTrigger() {
   );
 }
 
-function SettingsContent() {
+function Overlay({ className = '' }: { className?: string }) {
+  const { state } = useSettings();
+
+  return (
+    <div
+      data-state={state}
+      className={cn('fixed bottom-0 left-0 right-0 top-0 hidden data-[state=open]:block', className)}
+    ></div>
+  );
+}
+
+function SettingsContent({ children, className = '' }: { children: React.ReactNode; className?: string }) {
   const { state, setState } = useSettings();
   const modalRef = useRef<HTMLDivElement>(null);
 
@@ -45,12 +63,56 @@ function SettingsContent() {
       if (modalRef.current && !modalRef.current.contains(event.target as Node)) setState('closed');
     };
 
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') setState('closed');
+    };
+
     if (state === 'open') {
       document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener('keydown', handleEscape);
     }
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [state, setState]);
+
+  useEffect(() => {
+    if (state !== 'open' || !modalRef.current) return;
+
+    (document.activeElement as HTMLElement).blur();
+
+    const focusableElements = modalRef.current.querySelectorAll(
+      'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])',
+    );
+
+    const firstElement = modalRef.current.querySelector('input[name=theme]:checked') as HTMLElement;
+    const lastElement = focusableElements[focusableElements.length - 1] as HTMLElement;
+
+    const handleTab = (event: KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        firstElement.focus();
+        // 3 is temporary
+        if (focusableElements.length < 3) {
+          event.preventDefault();
+          return;
+        }
+
+        if (event.shiftKey && firstElement.contains(document.activeElement)) {
+          event.preventDefault();
+          lastElement.focus();
+        } else if (!event.shiftKey && lastElement.contains(document.activeElement)) {
+          event.preventDefault();
+          firstElement.focus();
+        }
+      }
+    };
+
+    document.addEventListener('keydown', handleTab);
+
+    return () => {
+      document.removeEventListener('keydown', handleTab);
     };
   }, [state, setState]);
 
@@ -58,16 +120,14 @@ function SettingsContent() {
     <div
       ref={modalRef}
       data-state={state}
-      className='absolute -bottom-2 right-0 translate-y-full rounded-md bg-muted p-4 data-[state=open]:block data-[state=closed]:hidden'
+      className={cn(
+        'absolute -bottom-2 right-0 z-10 translate-y-full rounded-md bg-muted p-4 data-[state=open]:block data-[state=closed]:hidden',
+        className,
+      )}
     >
-      <h2 className='text-center text-lg font-bold text-muted-foreground'>Settings</h2>
+      <h2 className='mb-2 text-center text-lg font-bold text-muted-foreground'>Settings</h2>
 
-      <ul className='mt-2'>
-        <li>
-          <h3 className='font-bold text-muted-foreground'>Theme</h3>
-          <ThemeSwitch className='mt-1' />
-        </li>
-      </ul>
+      {children}
     </div>
   );
 }
