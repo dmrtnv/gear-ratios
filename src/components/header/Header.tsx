1cf12f7f1f87b8c87380fbd39c32d549b9f92ee2
dmrtnv <dimasmart2000@gmail.com>
2024-10-03 11:04:51 +0200

    Add sticky position to Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,17 @@
+import { cn } from '@/lib/utils';
 import Settings from './Settings';
 
 function Header() {
   return (
-    <header className='flex w-full items-center justify-between border p-2'>
+    <header
+      className={cn(
+        'sticky z-10 flex w-full items-center justify-between border bg-background p-2',
+        // mobile
+        'top-0',
+        // desktop
+        'md:-top-14',
+      )}
+    >
       <span>Logo</span>
 
       <Settings />
